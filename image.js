6a64f0e91e3d3ccdd2444e0380f7e7eccba65833
vips11 <vipasha2002@gmail.com>
2022-05-13 01:25:27 -0400

    BR-2: inserted quotes api
    Chooses a random quote from the quotes API to post on Twitter

--- image.js
+++ image.js
@@ -1,27 +1,27 @@
 function DrawImage(text, path, textColor, backColor) {
-    const fs = require('fs')
-    const { createCanvas, loadImage } = require('canvas')
+  const fs = require('fs')
+  const { createCanvas, loadImage } = require('canvas')
 
-    const width = 1600
-    const height = 900
+  const width = 1600
+  const height = 900
 
-    const canvas = createCanvas(width, height)
-    const context = canvas.getContext('2d')
+  const canvas = createCanvas(width, height)
+  const context = canvas.getContext('2d')
 
-    context.fillStyle = backColor
-    context.fillRect(0, 0, width, height)
+  context.fillStyle = backColor
+  context.fillRect(0, 0, width, height)
 
-    context.font = 'bold 80pt Menlo'
-    context.textAlign = 'center'
-    context.textBaseline = 'top'
+  context.font = 'bold 80pt Menlo'
+  context.textAlign = 'center'
+  context.textBaseline = 'top'
 
-    const textWidth = context.measureText(text).width
-    context.fillRect(height - textWidth / 2, height / 2, textWidth, 400)
-    context.fillStyle = textColor
-    context.fillText(text, 800, 400)
+  const textWidth = context.measureText(text).width
+  context.fillRect(height - textWidth / 2, height / 2, textWidth, 400)
+  context.fillStyle = textColor
+  context.fillText(text, 800, 400)
 
-    const buffer = canvas.toBuffer('image/png')
-    fs.writeFileSync(path, buffer)
+  const buffer = canvas.toBuffer('image/png')
+  fs.writeFileSync(path, buffer)
 }
 
 DrawImage("Hello, World!", './test.png', '#CFD8DC', '#607D8B')
