a69bdae55ad04b288f748a479daa8c0ff628db0a
vips11 <66628544+vips11@users.noreply.github.com>
2022-05-17 02:17:44 -0400

    Update config.js

--- src/Platforms/Twitter/config.js
+++ src/Platforms/Twitter/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'wHxqtCEca7SaBYd9lngSfj5ZM',
-  consumer_secret: 'qFO1wt5wUvFPIoMUGGeLet65kFfF5gHGnHzNDGhcyaxAkYYXHh',
-  access_token: '1524853655752654853-GWXXrlpP0TIzCLgCetaR4c38Qct8kJ',
-  access_token_secret: 'YUozLjOKyBiGoLUXowqozviGV5CtWo3jDorLIOi1MvJgy'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
